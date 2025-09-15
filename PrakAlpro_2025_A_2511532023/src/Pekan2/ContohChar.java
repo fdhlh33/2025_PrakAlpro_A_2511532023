db15898475f8d4adb6d1d74c7a950f5bf7b3fae6
HP <HP@LABIF-22>
2025-09-15 14:46:40 +0700

    Praktikum 2 Kelas A

---
+++ PrakAlpro_2025_A_2511532023/src/Pekan2/ContohChar.java
@@ -0,0 +1,33 @@
+package Pekan2;
+
+public class ContohChar {
+	public static void main(String[] args) {
+		// Deklarasi variabel char
+		char huruf1 = 'A';
+		char huruf2 = 'B';
+		char angka = '7';
+		char simbol = '#';
+		
+		// Menampilkan nilai variabel char
+		System.out.println("Contoh variabel char:");
+		System.out.println("Huruf pertama: "+ huruf1);
+		System.out.println("Huruf kedua: " + huruf2);
+		System.out.println("Angka: " +angka);
+		System.out.println("Simbol: " +simbol);
+		
+		// Operasi dengan char: (berdasarkan kode unicode/ASCII)
+		char huruf3 = (char) (huruf1 + 1); // A (65) + 1 = B (65)
+		System.out.println("huruf1 + 1 =" + huruf3);
+		
+		// Char juga bisa disimpan dalam integer (ASCII/unicode value)
+		int kodehuruf = huruf1;
+		String biner1 = String.format("%8s", Integer.toBinaryString(huruf1)).replace(' ', '0');
+		System.out.println("Kode ASCII dari " + huruf1 + " = " + kodehuruf);
+		System.out.println("Kode BINER dari " + huruf1 + " = " + biner1);
+		
+		// Menggabungkan char menjadi string 
+		String kata = "" + huruf1 + huruf2 + angka + simbol;
+		System.out.println("Gabungan char menjadi string " + kata);
+	}
+
+}
---
+++ PrakAlpro_2025_A_2511532023/src/Pekan2/KelilingLingkaran.java
@@ -0,0 +1,10 @@
+package Pekan2;
+
+public class KelilingLingkaran {
+	public static void main(String[] args) {
+			final double PI = 3.14; /* Defenisi konstanta */
+			double radius = 30;     /* Deklarasi variabel */
+			System.out.println("Keliling ="+2*PI*radius);
+	}	
+	
+}
