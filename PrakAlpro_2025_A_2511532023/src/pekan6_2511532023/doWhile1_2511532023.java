7e63ca9dcffc248c1ca33bfe6c9dc45544316b0d
fdhlh33 <amiratulfadhilah@gmail.com>
2025-11-03 14:40:49 +0700

    Praktikum 6 Kelas A

---
+++ PrakAlpro_2025_A_2511532023/src/pekan6_2511532023/doWhile1_2511532023.java
@@ -0,0 +1,15 @@
+package pekan6_2511532023;
+
+import java.util.Scanner;
+
+public class doWhile1_2511532023 {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		String phrase;
+		do {
+			System.out.print("Input Password: ");
+			phrase = console.next();
+		} while (!phrase.equals("abcd"));
+	}
+
+}
