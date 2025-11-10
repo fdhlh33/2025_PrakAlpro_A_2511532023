243327aa08e1372c95b4b13764c91f87409ac915
fdhlh33 <amiratulfadhilah@gmail.com>
2025-11-10 14:43:47 +0700

    Praktikum 7 Kelas A

---
+++ PrakAlpro_2025_A_2511532023/src/pekan7_2511532023/PanggilMahasiswa_2511532023.java
@@ -0,0 +1,12 @@
+package pekan7_2511532023;
+
+public class PanggilMahasiswa_2511532023 {
+	public static void main(String[] args) {
+		Mahasiswa_2511532023 a= new Mahasiswa_2511532023();
+		a.setNim(23532);
+		a.setNama("Rahmat");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+}
